39755433465d4460d266a31e0be912f9fb3a7666
Ivan Panussis C <ivan.panussis.ing@gmail.com>
2023-04-19 00:07:53 +0100

    OpenJira initial design finished with Material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import '@/styles/globals.css';
+import "@fontsource/roboto";
+import { darkTheme, lightTheme } from '@/themes';
+import { UIProvider } from '@/context/ui';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <UIProvider>
+      <ThemeProvider theme={ darkTheme }>
+        <CssBaseline />
+        <Component {...pageProps} /> 
+      </ThemeProvider>
+    </UIProvider>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="shortcut" 
+          href='/favicon.ico'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
